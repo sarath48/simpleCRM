d319e5c0940588f80e7eb550ef579ec7a5a0c194
sarath <sarathsanthosh480@gmail.com>
2021-11-02 18:51:17 +0530

    worked on templates and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simpleCRM';
+  superPower:String="Hulk";
+  success:boolean=true;
+  contacts=[
+    {
+      "firstName":"Sarath",
+      "LastName":"Santhosh",
+      "Age":24
+    },
+    {
+      "firstName":"Sarath1",
+      "LastName":"Santhosh2",
+      "Age":25
+    },
+    {
+      "firstName":"Sarath2",
+      "LastName":"Santhosh2",
+      "Age":26
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
